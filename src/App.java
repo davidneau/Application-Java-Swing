4ae89c88d8573d72feb2b90d838177aa5f24f1a5
DaProg <davidneau49@gmail.com>
2019-08-19 21:41:53 +0100

    Création d'une animation (une boule qui rebondit sur les bords de la fenetre)

--- src/App.java
+++ src/App.java
@@ -9,29 +9,99 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 class Panneau extends JPanel {
+	  private int posX = -50;
+	  private int posY = -50;
+
 	  public void paintComponent(Graphics g){
-	    try {
-	      Image img = ImageIO.read(new File("C:\\Users\\david\\Documents\\Franck.png"));
-	      //g.drawImage(img, 0, 0, this);
-	      //Pour une image de fond
-	      g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
-	    } catch (IOException e) {
-	      e.printStackTrace();
-	    }                
-	  }               
+		g.setColor(Color.white);
+        g.fillRect(0, 0, this.getWidth(), this.getHeight());
+		g.setColor(Color.red);
+	    g.setColor(Color.red);
+	    g.fillOval(posX, posY, 50, 50);
+	  }
+
+	  public int getPosX() {
+	    return posX;
+	  }
+
+	  public void setPosX(int posX) {
+	    this.posX = posX;
+	  }
+
+	  public int getPosY() {
+	    return posY;
+	  }
+
+	  public void setPosY(int posY) {
+	    this.posY = posY;
+	  }        
 	}
 
 class Fenetre extends JFrame {
+	private Panneau pan = new Panneau();
+	
 	  public Fenetre(){
 	    this.setTitle("Ma première fenêtre Java");
-	    this.setSize(400, 500);
+	    this.setSize(300, 300);
 	    this.setLocationRelativeTo(null);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    JPanel pan = new JPanel();
-	    pan.setBackground(Color.BLUE);
-	    this.setContentPane(new Panneau());               
+	    this.setContentPane(pan);               
 	    this.setVisible(true);
+	    go();
 	  }
+	  
+	  private void go(){
+		  //Les coordonnées de départ de notre rond
+		  int x = pan.getPosX(), y = pan.getPosY();
+		  //Le booléen pour savoir si l'on recule ou non sur l'axe x
+		  boolean backX = false;
+		  //Le booléen pour savoir si l'on recule ou non sur l'axe y
+		  boolean backY = false;
+
+		  //Dans cet exemple, j'utilise une boucle while
+		  //Vous verrez qu'elle fonctionne très bien
+		  while(true){
+		    //Si la coordonnée x est inférieure à 1, on avance
+		    if(x < 1)
+		      backX = false;
+
+		    //Si la coordonnée x est supérieure à la taille du Panneau moins la taille du rond, on recule
+		    if(x > pan.getWidth()-50)
+		      backX = true;
+
+		    //Idem pour l'axe y
+		    if(y < 1)
+		      backY = false;
+		    if(y > pan.getHeight()-50)
+		      backY = true;
+
+		    //Si on avance, on incrémente la coordonnée
+		    //backX est un booléen, donc !backX revient à écrire
+		    //if (backX == false)
+		    if(!backX)
+		      pan.setPosX(++x);
+
+		    //Sinon, on décrémente
+		    else
+		      pan.setPosX(--x);
+
+		    //Idem pour l'axe Y
+		    if(!backY)
+		      pan.setPosY(++y);
+		    else
+		      pan.setPosY(--y);
+
+		    //On redessine notre Panneau
+		    pan.repaint();
+
+		    //Comme on dit : la pause s'impose ! Ici, trois millièmes de seconde
+		    try {
+		      Thread.sleep(1);
+		    } catch (InterruptedException e) {
+		      e.printStackTrace();
+		    }
+		  }
+		}   
 	}
 
 public class App {
