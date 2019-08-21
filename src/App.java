d0a41a4ad619e984dc3c0e42fae5a1501c9598c4
DaProg <davidneau49@gmail.com>
2019-08-21 11:54:11 +0100

    Bouttons

--- src/App.java
+++ src/App.java
@@ -1,12 +1,9 @@
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
 import java.awt.Color; 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import java.awt.Graphics;
-import java.awt.Image;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
 class Panneau extends JPanel {
 	  private int posX = -50;
@@ -37,71 +34,66 @@ class Panneau extends JPanel {
 	  }        
 	}
 
-class Fenetre extends JFrame {
-	private Panneau pan = new Panneau();
-	
+class Fenetre extends JFrame{
+	  private Panneau pan = new Panneau();
+	  private JButton bouton = new JButton("mon bouton");
+	  private JPanel container = new JPanel();
+	  
 	  public Fenetre(){
-	    this.setTitle("Ma première fenêtre Java");
+	    this.setTitle("Animation");
 	    this.setSize(300, 300);
-	    this.setLocationRelativeTo(null);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    this.setContentPane(pan);               
-	    this.setVisible(true);
+	    this.setLocationRelativeTo(null);
+	    container.setBackground(Color.white);
+	    container.setLayout(new BorderLayout());
+	    container.add(pan, BorderLayout.CENTER);
+	    container.add(bouton, BorderLayout.SOUTH);
+	    this.setContentPane(container);
+	    this.setVisible(true);    
 	    go();
 	  }
 	  
-	  private void go(){
-		  //Les coordonnées de départ de notre rond
-		  int x = pan.getPosX(), y = pan.getPosY();
-		  //Le booléen pour savoir si l'on recule ou non sur l'axe x
-		  boolean backX = false;
-		  //Le booléen pour savoir si l'on recule ou non sur l'axe y
-		  boolean backY = false;
-
-		  //Dans cet exemple, j'utilise une boucle while
-		  //Vous verrez qu'elle fonctionne très bien
-		  while(true){
-		    //Si la coordonnée x est inférieure à 1, on avance
-		    if(x < 1)
-		      backX = false;
-
-		    //Si la coordonnée x est supérieure à la taille du Panneau moins la taille du rond, on recule
-		    if(x > pan.getWidth()-50)
-		      backX = true;
-
-		    //Idem pour l'axe y
-		    if(y < 1)
-		      backY = false;
-		    if(y > pan.getHeight()-50)
-		      backY = true;
-
-		    //Si on avance, on incrémente la coordonnée
-		    //backX est un booléen, donc !backX revient à écrire
-		    //if (backX == false)
-		    if(!backX)
-		      pan.setPosX(++x);
-
-		    //Sinon, on décrémente
-		    else
-		      pan.setPosX(--x);
-
-		    //Idem pour l'axe Y
-		    if(!backY)
-		      pan.setPosY(++y);
-		    else
-		      pan.setPosY(--y);
-
-		    //On redessine notre Panneau
-		    pan.repaint();
-
-		    //Comme on dit : la pause s'impose ! Ici, trois millièmes de seconde
-		    try {
-		      Thread.sleep(1);
-		    } catch (InterruptedException e) {
-		      e.printStackTrace();
-		    }
-		  }
-		}   
+	  private void go(){  
+	    //Les coordonnées de départ de notre rond
+	    int x = pan.getPosX(), y = pan.getPosY();
+	    //Le booléen pour savoir si l'on recule ou non sur l'axe x
+	    boolean backX = false;
+	    //Le booléen pour savoir si l'on recule ou non sur l'axe y
+	    boolean backY = false;
+	    
+	    //Dans cet exemple, j'utilise une boucle while
+	    //Vous verrez qu'elle fonctionne très bien
+	    while(true){
+	      //Si la coordonnée x est inférieure à 1, on avance
+	      if(x < 1)backX = false;
+	      //Si la coordonnée x est supérieure à la taille du Panneau moins la taille du rond, on recule
+	      if(x > pan.getWidth()-50)backX = true;
+	      //Idem pour l'axe y
+	      if(y < 1)backY = false;
+	      if(y > pan.getHeight()-50)backY = true;
+	      
+	      //Si on avance, on incrémente la coordonnée
+	      if(!backX)
+	        pan.setPosX(++x);
+	      //Sinon, on décrémente
+	      else
+	        pan.setPosX(--x);
+	      //Idem pour l'axe Y
+	      if(!backY)
+	        pan.setPosY(++y);
+	      else
+	        pan.setPosY(--y);
+	        
+	      //On redessine notre Panneau
+	      pan.repaint();
+	      //Comme on dit : la pause s'impose ! Ici, trois millièmes de seconde
+	      try {
+	        Thread.sleep(3);
+	      } catch (InterruptedException e) {
+	        e.printStackTrace();
+	      }
+	    }    
+	  }  
 	}
 
 public class App {
