6eea86e64162bca3361a3cbfd52b7f0c8d2b1544
DaProg <davidneau49@gmail.com>
2019-08-25 16:22:11 +0100

    Bouttons

--- src/App.java
+++ src/App.java
@@ -1,9 +1,97 @@
 import java.awt.BorderLayout;
-import java.awt.Color; 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import java.awt.Graphics;
+
+class Bouton extends JButton implements MouseListener{
+	  private String name;
+	  private Image img;
+	  public Bouton(String str){
+	    super(str);
+	    this.name = str;
+	    try {
+	      img = ImageIO.read(new File("Moi.jpg"));
+	    } catch (IOException e) {
+	      e.printStackTrace();
+	    }
+	    //Grâce à cette instruction, notre objet va s'écouter
+	    //Dès qu'un événement de la souris sera intercepté, il en sera averti
+	    this.addMouseListener(this);
+	  }
+
+	  public void paintComponent(Graphics g){
+	    Graphics2D g2d = (Graphics2D)g;
+	    GradientPaint gp = new GradientPaint(0, 0, Color.blue, 0, 20, Color.cyan, true);
+	    g2d.setPaint(gp);
+	    g2d.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), this);
+	    g2d.setColor(Color.black);
+	    g2d.drawString(this.name, this.getWidth() / 2 - (this.getWidth() /  2 /4), (this.getHeight() / 2) + 5);
+	  }
+
+	  //Méthode appelée lors du clic de souris
+	  public void mouseClicked(MouseEvent event) { }
+
+	  public void mouseEntered(MouseEvent event) {
+		    //Nous changeons le fond de notre image pour le jaune lors du survol, avec le fichier fondBoutonHover.png
+		    try {
+		      img = ImageIO.read(new File("FW.jpg"));
+		    } catch (IOException e) {
+		      e.printStackTrace();
+		    }
+		  }
+
+	  public void mouseExited(MouseEvent event) {
+		  //Nous changeons le fond de notre image pour le vert lorsque nous quittons le bouton, avec le fichier fondBouton.png
+		    try {
+		      img = ImageIO.read(new File("Moi.jpg"));
+		    } catch (IOException e) {
+		      e.printStackTrace();
+		    }
+		  }
+
+		  public void mousePressed(MouseEvent event) {
+		    //Nous changeons le fond de notre image pour le jaune lors du clic gauche, avec le fichier fondBoutonClic.png
+		    try {
+		      img = ImageIO.read(new File("gs.jpg"));
+		    } catch (IOException e) {
+		      e.printStackTrace();
+		    }
+		  }
+		 
+		  public void mouseReleased(MouseEvent event) {
+			  //Nous changeons le fond de notre image pour le orange lorsque nous relâchons le clic avec le fichier fondBoutonHover.png si la souris est toujours sur le bouton
+			  if((event.getY() > 0 && event.getY() < this.getHeight()) && (event.getX() > 0 && event.getX() < this.getWidth())){
+			    try {
+			      img = ImageIO.read(new File("FW.jpg"));
+			    } catch (IOException e) {
+			      e.printStackTrace();
+			    }
+			  }
+			  //Si on se trouve à l'extérieur, on dessine le fond par défaut
+			  else{
+			    try {
+			      img = ImageIO.read(new File("Moi.jpg"));
+			    } catch (IOException e) {
+			      e.printStackTrace();
+			    }
+			  }               
+			}       
+		}
 
 class Panneau extends JPanel {
 	  private int posX = -50;
@@ -35,65 +123,85 @@ class Panneau extends JPanel {
 	}
 
 class Fenetre extends JFrame{
+
 	  private Panneau pan = new Panneau();
-	  private JButton bouton = new JButton("mon bouton");
+	  private JButton bouton = new JButton("Go");
+	  private JButton bouton2 = new JButton("Stop");
 	  private JPanel container = new JPanel();
-	  
+	  private JLabel label = new JLabel("Le JLabel");
+	  private int compteur = 0;
+	  private boolean animated = true;
+	  private boolean backX, backY;
+	  private int x, y;
+
 	  public Fenetre(){
 	    this.setTitle("Animation");
 	    this.setSize(300, 300);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    this.setLocationRelativeTo(null);
+
 	    container.setBackground(Color.white);
 	    container.setLayout(new BorderLayout());
 	    container.add(pan, BorderLayout.CENTER);
-	    container.add(bouton, BorderLayout.SOUTH);
+	    bouton.addActionListener(new BoutonListener()); 
+	    bouton.setEnabled(false);
+	    bouton2.addActionListener(new Bouton2Listener());
+
+	    JPanel south = new JPanel();
+	    south.add(bouton);
+	    south.add(bouton2);
+	    container.add(south, BorderLayout.SOUTH);
+	    Font police = new Font("Tahoma", Font.BOLD, 16);
+	    label.setFont(police);
+	    label.setForeground(Color.blue);
+	    label.setHorizontalAlignment(JLabel.CENTER);
+	    container.add(label, BorderLayout.NORTH);
 	    this.setContentPane(container);
-	    this.setVisible(true);    
+	    this.setVisible(true);
 	    go();
 	  }
-	  
-	  private void go(){  
+
+	  private void go(){
 	    //Les coordonnées de départ de notre rond
-	    int x = pan.getPosX(), y = pan.getPosY();
-	    //Le booléen pour savoir si l'on recule ou non sur l'axe x
-	    boolean backX = false;
-	    //Le booléen pour savoir si l'on recule ou non sur l'axe y
-	    boolean backY = false;
-	    
+	    x = pan.getPosX();
+	    y = pan.getPosY();
 	    //Dans cet exemple, j'utilise une boucle while
 	    //Vous verrez qu'elle fonctionne très bien
-	    while(true){
-	      //Si la coordonnée x est inférieure à 1, on avance
+	    while(this.animated){
 	      if(x < 1)backX = false;
-	      //Si la coordonnée x est supérieure à la taille du Panneau moins la taille du rond, on recule
-	      if(x > pan.getWidth()-50)backX = true;
-	      //Idem pour l'axe y
+	      if(x > pan.getWidth()-50)backX = true;          
 	      if(y < 1)backY = false;
 	      if(y > pan.getHeight()-50)backY = true;
-	      
-	      //Si on avance, on incrémente la coordonnée
-	      if(!backX)
-	        pan.setPosX(++x);
-	      //Sinon, on décrémente
-	      else
-	        pan.setPosX(--x);
-	      //Idem pour l'axe Y
-	      if(!backY)
-	        pan.setPosY(++y);
-	      else
-	        pan.setPosY(--y);
-	        
-	      //On redessine notre Panneau
+	      if(!backX)pan.setPosX(++x);
+	      else pan.setPosX(--x);
+	      if(!backY) pan.setPosY(++y);
+	      else pan.setPosY(--y);
 	      pan.repaint();
-	      //Comme on dit : la pause s'impose ! Ici, trois millièmes de seconde
+
 	      try {
 	        Thread.sleep(3);
 	      } catch (InterruptedException e) {
 	        e.printStackTrace();
 	      }
-	    }    
-	  }  
+	    }     
+	  }
+
+	  class BoutonListener implements ActionListener{
+	    public void actionPerformed(ActionEvent arg0) {
+	      animated = true;
+	      bouton.setEnabled(false);
+	      bouton2.setEnabled(true);
+	      go();
+	    }
+	  }
+
+	  class Bouton2Listener implements ActionListener{
+	     public void actionPerformed(ActionEvent e) {
+	      animated = false;     
+	      bouton.setEnabled(true);
+	      bouton2.setEnabled(false);
+	    }
+	  }     
 	}
 
 public class App {
